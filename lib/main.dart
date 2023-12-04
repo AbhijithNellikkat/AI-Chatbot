49592eb5d2d1d1693c856dc7878731cb6e0a572d
Abhijith Nellikkat <abhijithnellikkat@gmail.com>
2023-12-04 22:53:48 +0530

    create ai chatbot ui screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ai_chatbot_app/chat_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,12 +11,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'AI CHAT BOT',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.black, primary: Colors.black,),
+          seedColor: Colors.black,
+          primary: Colors.black,
+        ),
         useMaterial3: true,
       ),
+      home: ChatScreen(),
     );
   }
 }
